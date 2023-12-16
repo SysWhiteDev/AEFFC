1c5eddd8acb385b6ed2eafa364d73d2eef7ee5a8
SysWhiteDev <syswhite.commercial@gmail.com>
2023-12-16 23:50:21 +0100

    Vercel db update

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,13 +1,18 @@
-import pg from 'pg'
-import 'dotenv/config'
+import pg from 'pg';
+import 'dotenv/config';
+const { Pool } = pg;
 
-const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: Number(process.env.DB_PORT),
-})
+const db = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+
+// const db = new pg.Client({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS,
+//     port: Number(process.env.DB_PORT),
+// })
 
 db.connect((err: any) => {
     if (err) {
